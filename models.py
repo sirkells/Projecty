cd9bb11ed28e17c10f4683eee98a89f2e099d298
Kelechi Igbokwe <kells@Kelechis-MacBook-Pro.local>
2018-07-09 18:17:25 +0200

    Added config

--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from flask import current_app
-from blog import db, login_manager, app
+from app import db, login_manager, app
 from flask_login import UserMixin
 
 

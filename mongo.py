bbded2fc617df2098225d4159ff2b6e4fcbc3638
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2018-07-18 15:31:55 +0200

    Refactored app.py

--- mongo.py
+++ mongo.py
@@ -105,3 +105,64 @@ def search_request():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+"""
+ for hit in result['hits']['hits']:
+
+
+    docs = [{
+            'source': doc['_source'],
+            'score': doc['_score'],
+            'id': doc['_id']
+        } for doc in result['hits']['hits']]
+    projects = [{
+        'id': hit['id'],
+        'title': hit['source']['title'],
+        'description': hit['source']['description'][:600],
+        'filter_date_post': datetime.strptime(hit['source']['filter_date_post'], '%Y-%m-%dT%H:%M:%S') if result['source']['filter_date_post'] else datetime.utcnow(),
+        #'cockpit': True if hit['id'] in cockpit_set else False,
+        'url': hit['source']['url'],
+        'count': hit['source']['person_count'],
+        'duration': hit['source']['duration'],
+        'location': hit['source']['location'],
+        'source': hit['source']['source'],
+        'score': '{:.2f}'.format(hit['score'])
+                } ]
+    doc = {
+            "size" : 50,
+            "query": {
+              "multi_match": {
+                "query": search_term,
+                "fields": ["description", "title", "skills", "category"]
+              }
+            }
+          }"""
+
+    #res = es.search(index="projectfinder", body=doc)
+
+    """@app.route('/search/results', methods=['GET', 'POST'])
+def search_request():
+    search_term = request.form["input"]
+    res = es.search(index="flask_reminders", size=20, body={"query": {"multi_match" : { "query": search_term, "fields": ["description", "title", "skill_summary"] }}})
+    return render_template('results.html', res=res )"""
+
+#results = es.search(index="itproject", size=20, body={"query": {"multi_match" : { "query": search_term, "fields": ["description", "title", "skill_summary"] }}})
+    #results = handle.itproject.find( { "$text": { "$search": search_term, "$language": "de" } }, { "score": {"$meta": "textScore" } } )
+    #results.sort([('score', {'$meta': 'textScore'}), ("filter_date_post", -1)]).limit(30)
+    #results.sort([('score', {'$meta': 'textScore'}), ("filter_date_post", 1)]).limit(30)
+    #{ date: 1, score: { $meta: "textScore" } }
+    #results.sort('score', {'$meta': 'textScore'}, 'datefiel', -1).limit(100)
+    #result = es.search(index="projectfinder", body={"query": {"multi_match" : { "query": search_term, "fields": ["description", "title", "skills", "category"] }}})
+    #count = result['hits']['total']
+
+    #return render_template('home.html', projects=projects, count=count)
+    """def home():
+        projects = handle.itproject.find().sort([("filter_date_post", -1)].limit(150)
+        count = handle.itproject.find().count()
+        return render_template('home.html', projects=projects, count=count)"""
+
+#page_size = 30
+    #project = handle.itproject.find().limit(page_size)
+    #projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
+    #count = handle.itproject.find().count()

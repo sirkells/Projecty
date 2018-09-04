9c288d45e22fc6572456b94997204ee8affb2c54
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2018-09-04 14:51:59 +0200

    shorter automated stack feature

--- web/reset.py
+++ web/reset.py
@@ -15,5 +15,13 @@ db = connect()
 db.itproject.update({"region":{"$exists": True }}, {"$unset":{"region": ""}}, multi=True)
 #unset stack
 db.itproject.update({"stack":{"$exists": True }}, {"$unset":{"stack": ""}}, multi=True)
-db.itproject.update({"ham":{"$exists": True }}, {"$unset":{"ham": ""}}, multi=True)
-db.itproject.update({"zam":{"$exists": True }}, {"$unset":{"zam": ""}}, multi=True)
+
+
+
+#db.itproject.update({"skill_summary": {"$regex": 'node'}}, {"skill_summary": {"$regex": '(js|javascript|react|angular|vue|css|express|html|bootstrap)'}]}, "title":{"$regex": '(Node|Fullstack Javascript)', "$options": 'i'}}, {"$set":{"stack": "Fullstack"}}, multi=True)
+
+db.itproject.update({"skill_summary": {"$all": [{"$regex": 'node'}, {"$regex": '(^js|javascript|react|angular|vue|css|express|html|bootstrap)'}]},"title":{"$regex": '(Node|Fullstack Javascipt)', "$options": 'i'}}, {"$set":{"stack": "Node Fullstack"}}, multi=True)
+db.itproject.update({"skill_summary": {"$all": [{"$regex": 'node'}, {"$regex": '(^js|javascript|react|angular|vue|css|express|html|bootstrap)'}]}}, {"$set":{"stack": "Node Fullstack"}}, multi=True)
+
+#{$and: [ { skill_summary: RegExp(/node/i) }, { skill_summary: RegExp(/^js/) } ]}
+#{skill_summary: {$all: [RegExp(/node/), RegExp(/^js/)]}}

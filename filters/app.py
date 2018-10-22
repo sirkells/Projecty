9e318701d99cf84f8b17173ab5d45062120140dd
Kelechi Igbokwe <kelechi_igbokwe@yahoo.com>
2018-10-22 17:35:12 +0200

    added project count(amounts) to the api

--- filters/app.py
+++ filters/app.py
@@ -15,24 +15,51 @@ def connect():
 
 
 app = Flask(__name__)
+"""jinja_options = app.jinja_options.copy()
+jinja_options.update(dict(
+    variable_start_string = '(%',
+    variable_end_string = '%)'
+))
+app.jinja_options = jinja_options"""
+
 moment = Moment(app)
 CORS(app)
 db = connect()
-a = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": "Development"}).count()
-
+#a = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": "Development"}).count()
+category = ["Development", "Infrastructure", "Data Science"]
+lengths = []
+for group in category:
+    a = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group}).count()
+    lengths.append(a)
 @app.route('/')
 def index():
-    h = db.itproject_clean.find({"region": {"$ne": None}})
-    return render_template('index2.html', groups=h)
+    section = ['Development', 'Infrastructure', 'Data science']
+    return render_template('index3.html', section=[sec for sec in section])
+
+
+
 @app.route('/home', methods=['GET'])
 def home():
     page_size = 30
     project = db.itproject_clean.find({"region": {"$ne": None}, "bereich": {"$ne": None} }).limit(page_size)
+    project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich": {"$ne": None} })
+    projects1 = sorted(project1, key=lambda p: p['filter_date_post'], reverse=True)
     projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
-    amount = project.count()
-    amounts = len(projects)
+    #amount = project.count()
+    amounts = len(projects1)
+    #amount2 = len(project)
+    ab = {"amount": amounts, "amount2": lengths}
+    #projects.insert(0,ab)
+    #print(type(projects))
+    b = {"amount": amounts, "amount2": lengths}
+    b.update({"project_lists": projects})
+    page_sanitized = json.dumps(json.loads(json_util.dumps(b)))
+    #print(type(page_sanitized))
 
-    page_sanitized = json.dumps(json.loads(json_util.dumps(projects)))
+
+
+
+
 
     #print(type(d))
     #print(page_sanitized)
@@ -44,13 +71,67 @@ def home():
 @app.route('/<group>')
 def dev(group):
     page_size = 30
-
+    global pro
     project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group}).limit(page_size)
+    project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group})
+    projects1 = sorted(project1, key=lambda p: p['filter_date_post'], reverse=True)
+    pro = len(projects1)
+
     projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
-    amount = project.count()
-    amounts = len(projects)
-    page_sanitized = json.dumps(json.loads(json_util.dumps(projects)))
+    #amount = project.count()
+    #amounts = len(projects)
+    amounts = len(projects1)
+    #amount2 = len(project)
+    ab = {"amount": amounts, "amount2": page_size}
+    #projects.insert(0,ab)
+    #print(type(projects))
+    b = {"amount": amounts, "amount2": page_size}
+    b.update({"project_lists": projects})
+    page_sanitized = json.dumps(json.loads(json_util.dumps(b)))
+
     return page_sanitized
+
+
+@app.route('/<group>/<groupType>')
+def bereich_group_type(group, groupType):
+    page_size = 30
+    project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType}).limit(page_size)
+    project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType})
+    projects1 = sorted(project1, key=lambda p: p['filter_date_post'], reverse=True)
+    projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
+    #amount = project.count()
+    #amounts = len(projects)
+    amounts = len(projects1)
+    #amount2 = len(project)
+    ab = {"amount": amounts, "amount2": page_size}
+    #projects.insert(0,ab)
+    #print(type(projects))
+    b = {"amount": amounts, "amount2": page_size}
+    b.update({"project_lists": projects})
+    page_sanitized = json.dumps(json.loads(json_util.dumps(b)))
+    return page_sanitized
+    #return render_template('home.html', projects=projects, amount=amount, amounts=amounts)
+
+@app.route('/<group>/<groupType>/<groupStack>')
+def bereich_group_type_stack(group, groupType, groupStack):
+    page_size = 30
+    project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "bereich.group_type_stack": groupStack}).limit(page_size)
+    project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "bereich.group_type_stack": groupStack})
+    projects1 = sorted(project1, key=lambda p: p['filter_date_post'], reverse=True)
+    projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
+    #amount = project.count()
+    #amounts = len(projects)
+    amounts = len(projects1)
+    #amount2 = len(project)
+    ab = {"amount": amounts, "amount2": page_size}
+    #projects.insert(0,ab)
+    #print(type(projects))
+    b = {"amount": amounts, "amount2": page_size}
+    b.update({"project_lists": projects})
+    page_sanitized = json.dumps(json.loads(json_util.dumps(b)))
+
+    return page_sanitized
+
 """
 
 @app.route('/<group>')
---
+++ filters/templates/static/js/app1.js
@@ -0,0 +1,124 @@
+/*const vm = new Vue({
+    el: '#app',
+    data: {
+      results: []
+    },
+    mounted() {
+      axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=2efcc4b0db5b4859979baa0f40156792")
+      .then(response => {this.results = response.data.results})
+      //axios.get("http://127.0.0.1:5000/")
+      //.then(response => {this.results = response.data})
+      //console.log(results)
+    }
+  });*/
+
+  const SECTIONS = "Development, Infrastructure, Data Science";
+
+  const BaseUrl = "http://127.0.0.1:5000/";
+  
+  
+  function buildUrl(url) {
+      return BaseUrl + url
+  }
+  
+  Vue.component('news-list', {
+      props: ['results'],
+      template: `
+          <section>
+  
+              <table class="ui very basic padded striped four column table accordion">
+                          
+                          <thead>
+  
+                                  <th class="thirteen wide">Titel</th>
+                                  <th class="one wide">Veröffentlichung</th>
+                                  <th class="one wide">Ort</th>
+                                  <!--            <th class="one wide">Kategorie</th> -->
+                                  <th class="one wide">Quelle</th>
+                                  <th class="one wide">Cockpit</th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <div v-for="posts in processedPosts">
+                                  <div v-for="post in posts">
+                                  <hr>
+  
+                                      <tr>
+  
+                                          <td>
+                                              <div class="title">
+                                              <a :href="post.url" target="_blank"><b>{{ post.title }}</b></a>
+                                              </div>
+                                              <div class="content">
+                                                  <div class="ui relaxed divided items">
+                                                      <div class="item">
+                                                          <div class="description">
+                                                              {{ post.description.slice(0,350) }}
+                                                          </div>
+                                                      </div>
+                                                  </div>
+                                                  <div class="ui relaxed divided items">
+                                                      <div class="item">
+                                                          <div class="description">
+                                                              Category: <b>{{ post.bereich.group }}</b>
+                                                          </div>
+                                                          <div class="description">
+                                                              Sub-Category: <b>{{ post.bereich.group_type }}</b>
+                                                          </div>
+                                                          <div class="description">
+                                                              Date Posted: <b>{{ post.date_post }}</b>
+                                                          </div>
+                                                          <div class="description">
+                                                              Bundesland <b>{{ post.region.bundesland }}</b>, Stadt: <b>{{ post.region.stadt }}</b>
+                                                          </div>
+  
+  
+  
+                                                      </div>
+                                                  </div>
+                                              </div>
+  
+                                          </td>
+                                      </tr>
+                                  </div>
+                              </div>
+                          </tbody>
+               </table>
+          </section>`,
+      computed: {
+          processedPosts() {
+              let posts = this.results;
+  
+              // Add image_url attribute
+  
+  
+              // Put Array into Chunks
+              let i, j, chunkedArray = [], chunk = 4;
+              for (i = 0, j = 0; i < posts.length; i += chunk, j++) {
+                  chunkedArray[j] = posts.slice(i, i + chunk);
+              }
+              return chunkedArray;
+          }
+      }
+  });
+  
+  const vm = new Vue({
+      el: '#app',
+      data: {
+          results: [],
+          sections: SECTIONS.split(', '), // create an array of the sections
+          section: 'home', // set default section to 'home'
+      },
+      mounted() {
+          this.getPosts(this.section);
+      },
+      methods: {
+          getPosts(section) {
+              let url = buildUrl(section);
+              axios.get(url).then((response) => {
+                  this.results = response.data;
+              }).catch(error => { console.log(error); });
+          }
+      }
+  });
+  

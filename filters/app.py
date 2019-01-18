5534a295ac97f6ca7b49b067b9dba2f4ead003e2
sirkells <kelechi_igbokwe@yahoo.com>
2019-01-18 11:46:42 +0100

    Added and Completed Authentication with JWT

--- filters/app.py
+++ filters/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, jsonify, abort, flash, json, request, current_app
+from flask import Flask, render_template, url_for, redirect, jsonify, abort, flash, json, request, current_app, make_response
 from pymongo import MongoClient
 from elasticsearch import Elasticsearch
 from datetime import datetime
@@ -7,9 +7,11 @@ from bson import json_util, ObjectId
 import requests
 from flask_cors import CORS
 from urllib.parse import unquote
-from datetime import datetime
+from datetime import datetime, timedelta
 from werkzeug.security import generate_password_hash, check_password_hash
 import bcrypt
+import jwt
+from functools import wraps
 def connect():
     connection = MongoClient('127.0.0.1', 27017)
     handle = connection["projectfinder"]
@@ -69,6 +71,36 @@ default = {
                 }
             }
          }
+def token_required(f):  
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        invalid_msg = {
+            'message': 'Invalid token. Registeration and / or authentication required',
+            'authenticated': False
+        }
+        expired_msg = {
+            'message': 'Expired token. Reauthentication required.',
+            'authenticated': False
+        }
+        auth_headers = request.headers.get('Authorization', '').split()
+        if len(auth_headers) != 2:
+            return jsonify(invalid_msg), 401
+        try:
+            token = auth_headers[1]
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            user = users.find({"Username": data['sub']})[0]
+            if not user:
+                raise RuntimeError('User not found')
+            return f(user, *args, **kwargs)
+        except jwt.ExpiredSignatureError:
+            return jsonify(expired_msg), 401 # 401 is Unauthorized HTTP status code
+        except (jwt.InvalidTokenError, Exception) as e:
+            print(e)
+            return jsonify(invalid_msg), 401
+
+    return decorated
+
 def UserExist(username):
     #return false if user doesnt exist
     if users.find({"Username":username}).count() == 0:
@@ -94,8 +126,9 @@ def verifyPw(username, password):
     #hashes the given pwd by the user and compares it with the saved hashed pwd. ret true if equal
     if bcrypt.hashpw(password.encode('utf8'), hashed_pw) == hashed_pw:
         return True
+    
     else:
-        return False
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'}), False
 #verify logindetails: returns  true is theres an error status message and False with NOne status msg
 def verifyLoginDetails(username, password):
     if not UserExist(username):
@@ -103,10 +136,13 @@ def verifyLoginDetails(username, password):
     correct_pw = verifyPw(username, password)
     if not correct_pw:
         return getStatusMsg(302, "Incorrect password" ), True
-
     return None, False
 @app.route('/api/login', methods=['GET', 'POST']) 
 def login():
+    # auth = request.authorization
+    # if not auth or not auth.username or not auth.password:
+    #     return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+    
     postedData = request.get_json()
     username = postedData["username"]
     password = postedData["password"]
@@ -114,9 +150,17 @@ def login():
     if error:
         return jsonify(retJson)
     #the value 0 means that mongodb should exclude password and _id when showing the user
-    retJson = users.find({"Username": username}, {"Password": 0, "_id": 0})[0]
-    retJson.update({"message": "Welcome Kelechi", "auth": True})
-    return jsonify(retJson)
+    token = jwt.encode({
+    'sub': username,
+    'iat':datetime.utcnow(),
+    'exp': datetime.utcnow() + timedelta(hours=8)},
+    app.config['SECRET_KEY'])
+    # print(a)
+    # retJson = users.find({"Username": username}, {"Password": 0, "_id": 0})[0]
+    # retJson.update({ 'token' : token.decode('UTF-8')})
+    return jsonify({ 'token' : token.decode('UTF-8')})
+   
+    
 @app.route('/api/register', methods=['POST', 'GET'])
 def register():
         #get data from user
@@ -128,7 +172,6 @@ def register():
             return jsonify(getStatusMsg(301, "username already taken"))
             #if user doesnt exist, hash password
         hashedpw = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-
         #insert user in db
         users.insert({
             "Username": username,
@@ -136,9 +179,9 @@ def register():
         })
         return jsonify(getStatusMsg(200, "account succesfully created"))
 @app.route('/api/cockpit', methods=['GET', 'POST']) 
+@token_required
 def cockpit():
     response_object = {'status': 'success'}
-    
     if request.method == 'POST':
         post_data = request.get_json()
         cockpit = db.Cockpit
@@ -165,8 +208,21 @@ def cockpit():
         b = {"project_lists": results}
         page_sanitized = json.dumps(json.loads(json_util.dumps(b)))
         return page_sanitized
+@app.route('/')
+@app.route('/api/logout', methods=['GET', 'POST'])
+def index():
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_data(cache=False)
+        print(post_data)
+        response_object = {'status': 'success'}
+        return jsonify(response_object)
+    else:
+        response_object = {'status': 'Unauthorised'}
+        return jsonify(response_object)
 @app.route('/api/')
-def api():
+@token_required
+def api(user):
     group = request.args.get('group')
     groupType = request.args.get('groupType')
     groupStack = request.args.get('groupStack')
@@ -479,7 +535,6 @@ def api():
         doc_type = 'itproject_clean',
         body=body
         )
-    #print(all[1:20]['_source']['region'])
     try:
         # clean up
         docs = [{
@@ -584,7 +639,8 @@ def api():
     return page_sanitized
 
 @app.route('/api/search/', methods=['GET', 'POST']) 
-def search_request():
+@token_required
+def search_request(user):
     global search_term
     search_term = request.args.get("search_term")
     if 'sort' in default:
@@ -673,270 +729,15 @@ def search_request():
             }
         for hit in result['aggregations']['Skill Filter']['buckets']]
     } ]
-    aa = [a['items'] for a in regionAgg]
-    sk = [a['items'] for a in skillAgg]
-
-    ab = []
-    ba = []
-    for b in aa:
-        for c in b:
-            a = c['key'] + '(' + str(c['count']) + ')'
-            ab.append(c)
-            ba.append(a)
-            for d,num in zip(ab,ba):
-                d['land'] = num
-    print(ab)
-
-    ab1 = []
-    ba1 = []
-    for b in sk:
-        for c in b:
-            a = c['key'] + '(' + str(c['count']) + ')'
-            ab1.append(c)
-            ba1.append(a)
-            for d,num in zip(ab1,ba1):
-                d['land'] = num
     allAggs = groupAgg + groupTypeAgg + groupStackAgg + skillAgg + regionAgg
     projects = sorted(projects, key=lambda p: p['score'], reverse=True)
     #remove duplicates
     projects_unique = { d['title']:d for d in projects }.values()
     amounts = result['hits']['total']
-    b = {"amount": amounts, "amount2": lengths, "project_lists": projects_unique, "AllAggs": allAggs, "Allregion": ab, "Allskill": ab1}
-    parsed = json.loads(json_util.dumps(b))
-    page_sanitized = json.dumps(parsed, indent=4)
-    return page_sanitized
-@app.route('/api/filter/', methods=['GET', 'POST']) 
-def filter_request():
-    global filter_list
-    filter_list = request.args.get("filter_list")
-    if 'sort' in default:
-        del default['sort']
-    default['query'] = {
-              "multi_match": {
-                "query": filter_list,
-                "operator": "and",
-                "fields": ["title^5", "description"],
-                "fuzziness" : "AUTO",
-                "prefix_length" : 2
-              }
-            }
-    body = default
-    result = es.search(
-        index='projectfinder',
-        doc_type = 'itproject_clean',
-        body=body
-        )
-    try:
-        # clean up
-        docs = [{
-            'source': doc['_source'],
-            'score': doc['_score'],
-            'id': doc['_id']
-        } for doc in result['hits']['hits'] if 'region' in doc['_source']]
-    except KeyError:
-        # return message
-        return render_template('noresult.html')
-    projects = [{
-        'id': hit['id'],
-        'title': hit['source']['title'],
-        'description': hit['source']['description'],
-        'filter_date_post': datetime.strptime(hit['source']['filter_date_post'], '%Y-%m-%dT%H:%M:%S') if hit['source']['filter_date_post'] else datetime.utcnow(),
-        #'cockpit': True if hit['id'] in cockpit_set else False,
-        'url': hit['source']['url'],
-        'count': hit['source']['person_count'],
-        'duration': hit['source']['duration'],
-        'region': hit['source']['region'],
-        'bereich': hit['source']['bereich'],
-        'source': hit['source']['source'],
-        'score': hit['score']
-                } for hit in docs]
-    regionAgg = [{
-        'title': "Bundesland",
-        'items': [
-            {
-            'key': hit['key'],
-            'count': hit['doc_count']
-            }
-        for hit in result['aggregations']['Region Filter']['buckets']]
-    } ]
-    groupAgg = [{
-        'title': "Category",
-        'items': [
-            {
-            'key': hit['key'],
-            'count': hit['doc_count']
-            }
-        for hit in result['aggregations']['Group']['buckets']]
-    } ]
-    groupTypeAgg = [{
-        'title': "Sub-Category",
-        'items': [
-            {
-            'key': hit['key'],
-            'count': hit['doc_count']
-            }
-        for hit in result['aggregations']['Group Type']['buckets']]
-    }]
-    groupStackAgg = [{
-        'title': "Stack",
-        'items': [
-            {
-            'key': hit['key'],
-            'count': hit['doc_count']
-            }
-        for hit in result['aggregations']['Group Stack']['buckets']]
-    } ]
-    skillAgg = [{
-        'title': "Skills",
-        'items': [
-            {
-            'key': hit['key'],
-            'count': hit['doc_count']
-            }
-        for hit in result['aggregations']['Skill Filter']['buckets']]
-    } ]
-    aa = [a['items'] for a in regionAgg]
-    sk = [a['items'] for a in skillAgg]
-
-    ab = []
-    ba = []
-    for b in aa:
-        for c in b:
-            a = c['key'] + '(' + str(c['count']) + ')'
-            ab.append(c)
-            ba.append(a)
-            for d,num in zip(ab,ba):
-                d['land'] = num
-    print(ab)
-
-    ab1 = []
-    ba1 = []
-    for b in sk:
-        for c in b:
-            a = c['key'] + '(' + str(c['count']) + ')'
-            ab1.append(c)
-            ba1.append(a)
-            for d,num in zip(ab1,ba1):
-                d['land'] = num
-    print(ab1)
-    allAggs = groupAgg + groupTypeAgg + groupStackAgg + skillAgg + regionAgg
-    projects = sorted(projects, key=lambda p: p['score'], reverse=True)
-    #remove duplicates
-    projects_unique = { d['title']:d for d in projects }.values()
-    amounts = result['hits']['total']
-    b = {"amount": amounts, "amount2": lengths, "project_lists": projects_unique, "AllAggs": allAggs, "aggRegion": regionAgg, "Allregion": ab, "Allskill": ab1 }
+    b = {"amount": amounts, "amount2": lengths, "project_lists": projects_unique, "AllAggs": allAggs}
     parsed = json.loads(json_util.dumps(b))
     page_sanitized = json.dumps(parsed, indent=4)
     return page_sanitized
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
-
-"""
-
-elif group  and bundesland and not groupStack and not groupType:
-        project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "region.bundesland": bundesland}).limit(100)
-        project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "region.bundesland": bundesland})
-    elif group and groupType and not groupStack and not bundesland:
-        project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType}).limit(100)
-        project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType})
-    elif group and groupType and bundesland and not groupStack:
-        project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "region.bundesland": bundesland}).limit(100)
-        project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "region.bundesland": bundesland})
-    elif group and groupType and groupStack and not bundesland:
-        project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "bereich.group_type_stack": groupStack}).limit(100)
-        project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "bereich.group_type_stack": groupStack})
-    elif group and groupType and groupStack and skill and bundesland:
-        project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "bereich.group_type_stack": groupStack, "bereich.skill": skill, "region.bundesland": bundesland}).limit(100)
-        project1 = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "bereich.group_type_stack": groupStack, "bereich.skill": skill, "region.bundesland": bundesland})
-
-
-
-
-
-
-
-@app.route('/query')
-def query():
-    language = request.args.get('language') #if key doesn't exist, returns None
-
-
-    #framework = request.args['framework'] #if key doesn't exist, returns a 400, bad request error
-    #website = request.args.get('website')
-    results = db.itproject_clean.find( { "region": {"$ne": None}, "bereich": {"$ne": None}, "$text": { "$search":  language, "$language": "de" } }, { "score": {"$meta": "textScore" } } )
-
-    results.sort([('score', {'$meta': 'textScore'}), ("filter_date_post", 1)]).limit(100)
-    projects = [p for p in results]
-    #sorted(results, key=lambda p: p['filter_date_post'], reverse=True)
-    print(type(projects))
-    amounts = len(projects)
-    b = {"amount": amounts, "amount2": lengths}
-    b.update({"project_lists": projects})
-    page_sanitized = json.dumps(json.loads(json_util.dumps(b)))
-
-    return page_sanitized
-@app.route('/search/<search_term>', methods=['GET', 'POST'])
-def search_request(search_term):
-    #search_term = request.form["input"]
-    #results = es.search(index="itproject", size=20, body={"query": {"multi_match" : { "query": search_term, "fields": ["description", "title", "skill_summary"] }}})
-    results = db.itproject_clean.find( { "region": {"$ne": None}, "bereich": {"$ne": None}, "$text": { "$search": search_term, "$language": "de" } }, { "score": {"$meta": "textScore" } } )
-    results.sort([('score', {'$meta': 'textScore'}), ("filter_date_post", 1)]).limit(100)
-    projects = [p for p in results]
-    #sorted(results, key=lambda p: p['filter_date_post'], reverse=True)
-    print(type(projects))
-    amounts = len(projects)
-    b = {"amount": amounts, "amount2": lengths}
-    b.update({"project_lists": projects})
-    page_sanitized = json.dumps(json.loads(json_util.dumps(b)))
-
-    #res = es.search(index="projectfinder", body=doc)
-    return page_sanitized
-
-
-
-
-@app.route('/<group>')
-def bereich_group(group):
-    page_size = 30
-
-    project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group}).limit(page_size)
-    projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
-    amount = project.count()
-    amounts = len(projects)
-
-    return render_template('home.html', projects=projects, amount=amount, amounts=amounts, selected=group)
-
-
-@app.route('/<group>/<groupType>')
-def bereich_group_type(group, groupType):
-    page_size = 30
-    project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType}).limit(page_size)
-    projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
-    amount = project.count()
-    amounts = len(projects)
-    return render_template('home.html', projects=projects, amount=amount, amounts=amounts)
-
-@app.route('/<group>/<groupType>/<groupStack>')
-def bereich_group_type_stack(group, groupType, groupStack):
-    page_size = 30
-    project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "bereich.group_type_stack": groupStack}).limit(page_size)
-    projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
-    amount = project.count()
-    amounts = len(projects)
-
-    return render_template('home.html', projects=projects, amount=amount, amounts=amounts)
-
-@app.route('/<group>/<groupType>/<groupStack>/<skill>')
-def bereich_skill(group, groupType, groupStack, skill):
-    page_size = 30
-    project = db.itproject_clean.find({"region": {"$ne": None}, "bereich.group": group, "bereich.group_type": groupType, "bereich.group_type_stack": groupStack, "bereich.skill": skill}).limit(page_size)
-    projects = sorted(project, key=lambda p: p['filter_date_post'], reverse=True)
-    amount = project.count()
-    amounts = len(projects)
-
-    return render_template('home.html', projects=projects, amount=amount, amounts=amounts)
-"""
